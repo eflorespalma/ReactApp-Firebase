2fd049687a5b698bfc0295480589af906e0fd563
Edgar Flores <eflorespalma@gmail.com>
2017-03-06 17:03:26 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import firebase from 'firebase';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    };
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogOut = this.handleLogOut.bind(this);
+  }
+
+
   handleAuth() {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider)
@@ -13,6 +23,31 @@ class App extends Component {
         console.log(`Error ${error.code}: ${error.message}`);
       });
   }
+
+  handleLogOut() {
+    firebase.auth().signOut()
+      .then((result) => {
+        console.log(`${result.user.email} is offline`);
+      })
+      .catch((error) => {
+        console.log(`Error ${error.code}: ${error.message}`);
+      });
+  }
+
+  renderLoginButton() {
+    if (this.state.user) {
+      return (
+        <div>
+          <img src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <p>Hola {this.state.user.displayName}</p>
+          <button onClick={this.handleLogOut}>Log Out</button>
+        </div>
+      );
+    }else{
+      return (<button onClick={this.handleAuth}>Login with Google</button>)
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,7 +55,7 @@ class App extends Component {
           <h2>Minigram</h2>
         </div>
         <p className="App-intro">
-          <button onClick={this.handleAuth}>Login with Google</button>
+          {this.renderLoginButton()}
         </p>
       </div>
     );
